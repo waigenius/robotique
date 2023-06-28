e4ad40ad1c5015c327f08575175f66b7c7c000a9
wai <wailekone@gmail.com>
2023-06-28 15:58:58 +0200

    bras robot fonctionne

---
+++ launch/robot_nano_hand.launch.py
@@ -0,0 +1,39 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    urdf_file_name = 'robot_nano_hand.urdf.xml'
+
+    urdf = os.path.join(get_package_share_directory('robot_nano_hand_teleop'),urdf_file_name)
+
+    with open (urdf, 'r') as infp:
+        robot_desc = infp.read()
+    
+    return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time',default_value = 'false', description = 'Use simulation (Gazebo) clock if true'),
+
+        Node(
+            package = 'robot_state_publisher',
+            executable = 'robot_state_publisher',
+            name = 'robot_state_publisher',
+            output = 'screen',
+            parameters = [{
+                'use_sim_time' : use_sim_time, 
+                'robot_description': robot_desc}],
+            arguments = [urdf]),
+
+        Node(
+            package ='robot_nano_hand_teleop',
+            executable = 'state_publisher',
+            name ='state_publisher',
+            output = 'screen'),
+        
+    ])
